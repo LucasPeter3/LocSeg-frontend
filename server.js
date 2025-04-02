93941ffeea9814951c2dc44d23e8d63c669c4d3c
LucasPeter3 <lucaspeterson11@hotmail.com>
2025-04-02 11:14:09 -0300

    Primeiro commit: versão inicial do BackScan

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const TELEGRAM_BOT_TOKEN = "BOT-TOKEN"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "CHAT-TOKEN"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_BOT_TOKEN = "7502823923:AAGHi3MH_WFqpmjHNshyWywpPIoHXfOfP_8"; // Substitua pelo token do seu bot
+const TELEGRAM_CHAT_ID = "-1002323957242"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
